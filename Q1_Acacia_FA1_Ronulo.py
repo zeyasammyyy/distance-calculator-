0bf7eeb905096681ca4d30b0b752355de649e199
zeyasammyyy <zsbronulo31@src.pshs.edu.ph>
2026-08-10 09:47:08 +0800

    Add files via upload

---
+++ Q1_Acacia_FA1_Ronulo.py
@@ -0,0 +1,9 @@
+import math
+x1 = float(input("Enter x1: "))         
+y1 = float(input("Enter y1: "))
+x2 = float(input("Enter x2: "))
+y2 = float(input("Enter y2: "))
+distance = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
+print("The distance between the two points is:", distance)
+
+#Using a library is more practical than writing all calculations because it saves time by using easy to use functions. It saved time and made the distance calculation clean and easy to read. If I wrote all calculations from scratch, it would've probably took a long time and I would've made mistakes.
